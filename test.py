199e05dc0278b40fc994fc08419d79a91c37d04a
Abdul4968 <59417680+Abdul4968@users.noreply.github.com>
2023-03-16 17:52:10 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-
-
-import urllib.request
-url = "https://raw.githubusercontent.com/Abdul4968/Abdul4968/main/test.py"
-code = urllib.request.urlopen(url).read().decode()
-print("---------------")
-exec(code)
