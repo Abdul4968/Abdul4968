c4bfcd576d8b523457538ce3149ff62422fc467c
Abdul4968 <59417680+Abdul4968@users.noreply.github.com>
2023-03-16 17:49:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,5 @@
 import urllib.request
 url = "https://raw.githubusercontent.com/Abdul4968/Abdul4968/main/test.py"
 code = urllib.request.urlopen(url).read().decode()
-print(code)
 print("---------------")
 exec(code)
